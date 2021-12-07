ead727b728a8203d52403283f9db2e61bb66b525
perdi padilah <77036605+PerdiPadilah@users.noreply.github.com>
2021-12-07 21:40:04 +0700

    Add files via upload

---
+++ Tugas4/tugas4.php
@@ -0,0 +1,55 @@
+<?php
+
+abstract class bidang{
+   abstract public function hitung_keliling ();
+}
+interface datar{
+ public function hitung_luas();
+}
+
+class persegi extends bidang implements datar{
+   public function hitung_keliling(){
+     return 4+4+4+4;
+   }
+    public function hitung_luas(){
+     return 4*4;
+   }
+}
+
+class p_panjang extends bidang implements datar{
+   public function hitung_keliling(){
+     return 4+8+4+8;
+   }
+   public function hitung_luas(){
+     return 4*8;
+   }
+}
+
+// buat objek dari class diatas
+$bidang_persegi = new persegi();
+$bidang_P_panjang = new P_panjang();
+
+// buat fungsi untuk memproses objek
+function keliling ($objek_bidang){
+   return $objek_bidang->hitung_keliling();
+}
+
+function luas($objek_bidang){
+   return $objek_bidang->hitung_luas();
+}
+
+// jalankan fungsi
+
+echo "keliling persegi = ";
+echo keliling($bidang_persegi);
+echo "<hr>";
+echo "luas persegi = ";
+echo luas($bidang_persegi);
+echo "<hr>";
+echo "keliling persegi panjang = ";
+echo keliling($bidang_P_panjang);
+echo "<hr>";
+echo "keliling persegi panjang = ";
+echo luas($bidang_P_panjang);
+
+?>
